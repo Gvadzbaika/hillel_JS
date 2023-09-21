2d2356b77cabc30293feeab05942d398a10ecc3d
nataly.gvadzbaya <nataly.gvadzbaya@ringcentral.com>
2023-09-21 17:29:17 +0200

    HW 9

--- src/main.js
+++ src/main.js
@@ -1,46 +1,16 @@
 //Task 1
 
-const words = ["apple", "banaNA", "kiWi", "ORANGE"];
+function showPrime(arr){
 
-function capitalizeStrings(arr){
-    const capitalizeStringsArr =[];
-    for (let word of arr) {
-        word = word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
-        capitalizeStringsArr.push(word);
-}
-return capitalizeStringsArr;
-}
-console.log(capitalizeStrings(words));
-
-
-// Task 2
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
-function findCommonElements(arr1, arr2){
-    return arr1.filter(function(n) {
-        return arr2.indexOf(n) !== -1;
-      });
-}
-
-console.log(findCommonElements(array1, array2)); // [3, 4, 5]
-
-//Task 3
-
-const numbers = [1, 2, 3, 4, 5];
-
-function analyzeArray(arr) {
-    const sum = arr.reduce((acc, num) => acc + num, 0);
-    const min = Math.min(...arr);
-    const max = Math.max(...arr);
-  
-    return {
-      sum : sum,
-      average: sum / arr.length,
-      min: min,
-      max: max,
-    };
+arr = arr.filter((number) => {
+  for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
   }
+  return true;
+});
 
-console.log(analyzeArray(numbers)); // { sum: 15, average: 3, min: 1, max: 5 }
+return arr;}
+
+arr = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10 ];
+console.log (showPrime(arr));
+
